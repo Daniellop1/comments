27ae9e2585d05fee13eafe3642423c53e96c36a2
Daniel <2005danielus@gmail.com>
2022-01-19 15:45:11 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,6 +28,8 @@ export async function getStaticProps({ locale }: GetStaticPropsContext) {
   const contents = readme.split('<!-- configuration -->');
   const [afterConfig] = contents[1].split('<!-- end -->');
 
+  contents[1] = `${afterConfig}\n## ${t('tryItOut')} 👇👇👇\n`;
+
   const token = await getAppAccessToken('giscus/giscus').catch(() => '');
   const [contentBefore, contentAfter] = await Promise.all(
     contents.map((section) => renderMarkdown(section, token, 'giscus/giscus')),
@@ -35,9 +37,9 @@ export async function getStaticProps({ locale }: GetStaticPropsContext) {
 
   const comment: IComment = {
     author: {
-      avatarUrl: 'https://avatars.githubusercontent.com/u/44546966',
-      login: 'daniellop1',
-      url: 'https://github.com/daniellop1',
+      avatarUrl: 'https://avatars.githubusercontent.com/in/106117',
+      login: 'giscus',
+      url: 'https://github.com/apps/giscus',
     },
     authorAssociation: 'APP',
     bodyHTML: contentBefore,
@@ -53,7 +55,7 @@ export async function getStaticProps({ locale }: GetStaticPropsContext) {
     replies: [],
     replyCount: 0,
     upvoteCount: 0,
-    url: 'https://github.com/daniellop1/comments',
+    url: 'https://github.com/giscus/giscus',
     viewerDidAuthor: false,
     viewerHasUpvoted: false,
     viewerCanUpvote: false,
@@ -123,22 +125,32 @@ export default function Home({
     <main className="w-full min-h-screen gsc-homepage-bg" data-theme={theme}>
       <div className="w-full max-w-3xl p-2 mx-auto color-text-primary">
         <Comment comment={comment}>
-
+          <Configuration
+            directConfig={directConfig}
+            onDirectConfigChange={handleDirectConfigChange}
+          />
           <div className="p-4 pt-0 markdown" dangerouslySetInnerHTML={{ __html: contentAfter }} />
         </Comment>
 
         <div className="w-full my-8 giscus" />
         <Script
           src="/client.js"
-          data-repo="daniellop1/comments"
-          data-repo-id="R_kgDOGmDcFQ"
-          data-category-id="DIC_kwDOGmDcFc4CAirK"
-          data-mapping="og:title"
-          ata-reactions-enabled="0"
-          data-emit-metadata="0"
+          data-repo="giscus/giscus"
+          data-repo-id="MDEwOlJlcG9zaXRvcnkzNTE5NTgwNTM="
+          data-category-id="MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyNzk2NTc1"
+          data-mapping="specific"
+          data-term="Welcome to giscus!"
           data-theme="light"
+          data-reactions-enabled="1"
+          data-emit-metadata="0"
           data-lang={locale}
         />
+        <a
+          className="block mx-auto mb-6 w-max"
+          href="https://vercel.com/?utm_source=giscus&utm_campaign=oss"
+        >
+          <img src="/powered-by-vercel.svg" alt="Powered by Vercel" />
+        </a>
       </div>
     </main>
   );
